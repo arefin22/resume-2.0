1c8b9fcc9b451558936d499e1c40e58010cc2285
Shamsul Arefin <shamsularefin500@gmail.com>
2024-04-01 13:43:20 +0600

    Homepage And Navbar Layout Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,46 @@
 import Image from "next/image";
-import { FaDownload } from "react-icons/fa6";
+import Link from "next/link";
+import { FaDownload, FaGithub, FaLinkedin } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-2 bg-[#313131]">
       <div className="bg-white hight flex flex-row justify-between">
         <div className="p-4 w-8/12">
-          <p>Hi There</p>
-          <div className="flex flex-row">
-            <p>I&apos;m&nbsp;</p>
-            <h1>Shamsul Arefin</h1>
-          </div>
-          <h2>Web Developer | Frontend Developer</h2>
-          <p className="w-6/12">
-            I am good at website front End development. I have experience on
-            next js, react js and redux. Also in recent projects I am using
-            Typescript to make my projects more reliable. I have some team
-            collaboration experience and I have worked some real life used
-            projects as freelancer.
-          </p>
-          <div>
-            <FaDownload />
-            <button>Download Resume</button>
-          </div>
-          <div className="flex flex-row gap-3 bg-center">
-            <div className="p-5 rounded-full bg-sky-900 w-10"></div>
-            <div className="p-5 rounded-full bg-sky-900 w-10"></div>
-            <div className="p-5 rounded-full bg-sky-900 w-10"></div>
+          <div className="flex flex-col justify-center items-center highet_screen gap-4">
+            <p className="text-2xl">Hi There</p>
+            <div className="flex flex-row font-bold text-4xl">
+              <p>I&apos;m&nbsp;</p>
+              <h1 className="text-[#1E436F] uppercase">Shamsul Arefin</h1>
+            </div>
+            <div className="w-4/12">
+              <h2 className="bg-[#1E436F] p-4 rounded-xl text-xl text-white font-semibold">
+                Web Developer | Frontend Developer
+              </h2>
+            </div>
+            <p className="w-6/12 text-center">
+              I am good at website front End development. I have experience on
+              next js, react js and redux. Also in recent projects I am using
+              Typescript to make my projects more reliable. I have some team
+              collaboration experience and I have worked some real life used
+              projects as freelancer.
+            </p>
+            <button className="flex flex-row gap-3 justify-center items-center bg-[#1E436F] hover:bg-[#000000] rounded-xl p-4 text-white font-semibold">
+              <FaDownload />
+              Download Resume
+            </button>
+            <div className="flex flex-row gap-3 bg-center">
+              <Link className="p-3 rounded-full bg-sky-900 text-3xl text-white hover:bg-[#000000]" href="https://www.linkedin.com/in/shamsularefinn/" target="_blank">
+                <FaLinkedin />
+              </Link>
+              <Link className="p-3 rounded-full bg-sky-900 text-3xl text-white hover:bg-[#000000]" target="_blank" href="https://github.com/arefin22" >
+                <FaGithub />
+              </Link>
+              <Link className="p-3 rounded-full bg-sky-900 text-3xl text-white hover:bg-[#000000]" target="_blank" href="mailto:$[shamsularefinn@gmail.com]" >
+                <MdEmail />
+              </Link>
+            </div>
           </div>
         </div>
         <div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { FaHome } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
+import { MdPermContactCalendar } from "react-icons/md";
+import { RiComputerFill } from "react-icons/ri";
+
+const Navbar = () => {
+  return (
+    <div className="w-2/12 relative mx-auto">
+      <ul className="flex flex-row absolute mt-4 text-xl text-white justify-center gap-9">
+        <li className="rounded-full p-5 bg-[#1E436F] hover:bg-[#000000]">
+          <Link href="#">
+            <FaHome />
+          </Link>
+        </li>
+        <li className="rounded-full p-5 bg-[#1E436F] hover:bg-[#000000]">
+          <Link href="#">
+            <IoPerson />
+          </Link>
+        </li>
+        <li className="rounded-full p-5 bg-[#1E436F] hover:bg-[#000000]">
+          <Link href="#">
+            <RiComputerFill />
+          </Link>
+        </li>
+        <li className="rounded-full p-5 bg-[#1E436F] hover:bg-[#000000]">
+          <Link href="#">
+            <MdPermContactCalendar />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
